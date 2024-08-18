95dc4b63f716d6d6a6cbb6b3ef1ea86444f10c89
chinky23-tech <chinky0330@gmail.com>
2024-08-18 13:50:32 +0530

    add script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('search-button').addEventListener('click',function(){
+    const query = document.getElementById('search-input').ariaValueMax;
+    alert('you search for:'  +query);
+});
