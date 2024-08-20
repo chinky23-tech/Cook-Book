ee4d9fe81cd67049f06143e6e03f3777d9892b1e
chinky23-tech <chinky0330@gmail.com>
2024-08-20 12:49:28 +0530

    fixed code

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ document.getElementById('search-button').addEventListener('click',function(){
     const query = document.getElementById('search-input').ariaValueMax;
     alert('you search for:'  +query);
 });
+
+
+//script .js
+document.getElementById('openSearchBtn').addEventListener("click", function(){
+    document.getElementById("fullscreensearch").style.display = "flex";
+
+});
+document.getElementById("closeSearchbtn").addEventListener("click",function(){
+    document.getElementById("fullscreensearch").style.display = "none";
+});
